1bbe3d3dd79ffd5a35ae1ccbb80ac1c9e2504b27
Watchvader <martinez.diego.1am@gmail.com>
2024-01-17 19:38:30 -0600

    se edito el contenido 2 para las imaganes de barcos

--- src/components/contenido2.js
+++ src/components/contenido2.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './contenido2.css';
 import Conten3 from './conten3';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 const Contenido2 = () => {
+  useEffect(() => {
+    Aos.init({ duration:1200 });
+  }, []);
   return (
     <div className="contenido2">
-      <div className="imagenes-izquierda6">
-        <img src="/images/b3.jpeg" alt="Imagen 3" className="imagen3" style={{ width: '27%' }} />
-        <img src="/images/b1.jpeg" alt="Imagen 1" className="imagen1" style={{ width: '20%', marginTop: '10px' }} />
-        <img src="/images/b2.jpeg" alt="Imagen 2" className="imagen2" style={{ width: '20%' }} />
-       
+      <div className="imagenen_principal" data-aos="fade-right" >
+        <img src="/images/b3.jpeg" alt="Imagen 3" className="imagen3"  />
       </div>
-      <div className="texto-sobreimagen">
-        <h1>sasas</h1>
+      <div className="imagenes" data-aos="fade-down">
+        <img src="/images/b1.jpeg" alt="Imagen 1" className="imagen1"  />
+        <img src="/images/b2.jpeg" alt="Imagen 2" className="imagen2"   />
+      </div>
+      <div className="texto-sobreimagen" data-aos="zoom-in-left">
+        <h1>La ubicación estratégica de nuestras instalaciones, a escasos minutos del puerto de Manzanillo, facilita de manera significativa la exportación de limones a nivel global. Esta proximidad al puerto, uno de los principales centros logísticos de la región, nos otorga una ventaja competitiva al permitirnos realizar envíos eficientes y oportunos a destinos internacionales. Estamos comprometidos con la excelencia en la logística de exportación, garantizando la frescura y calidad de nuestros productos en cualquier parte del mundo.</h1>
       </div>
     </div>
   );
